8507e54494a6c1c2682228c2c4e1c38a9b2380ea
5binn <dhxnejqls13@gmail.com>
2025-01-09 17:28:06 +0900

    reducet&context01

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
-import Heading from "./Heading";
-import Section from "./Section";
+import { useReducer } from "react";
+import { tasksReducer } from "./TaskReducer";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList";
+
+export interface Task {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+let nextId: number = 3;
+const initialTasks: Task[] = [
+  { id: 0, text: "Philosopher’s Path", done: true },
+  { id: 1, text: "Visit the temple", done: false },
+  { id: 2, text: "Drink matcha", done: false },
+];
 
 export default function App() {
-  return (
-    <Section>
-      <Heading>My Profile</Heading>
-      <Post title="Hello traveller!" body="Read about my adventures." />
-      <Allposts />
-    </Section>
-  );
-}
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
-function Allposts() {
-  return (
-    <Section>
-      <Heading>Posts</Heading>
-      <RecentPost></RecentPost>
-    </Section>
-  );
-}
+  function handleAddTask(text: string) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
 
-function RecentPost() {
-  return (
-    <Section>
-      <Heading>Recent Post</Heading>
-      <Post title="Flavors of Lisbon" body="...those pastéis de nata!" />
-      <Post title="Buenos Aires in the rhythm of tango" body="I loved it!" />
-    </Section>
-  );
-}
+  function handleChangeTask(task: Task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
 
-type Post = {
-  title: string;
-  body: string;
-};
+  function handleDeleteTask(taskId: number) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
 
-function Post({ title, body }: Post) {
   return (
-    <Section isFancy={true}>
-      <Heading>{title}</Heading>
-      <p>
-        <i>{body}</i>
-      </p>
-    </Section>
+    <>
+      <h1>Day off in Tokyo</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
   );
 }
