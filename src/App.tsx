38d809bae7a765acad24f4385aafdbe3ad5a8466
5binn <dhxnejqls13@gmail.com>
2025-01-09 16:58:36 +0900

    context04

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,44 @@ import Section from "./Section";
 export default function App() {
   return (
     <Section>
-      <Heading>Title</Heading>
-      <Section>
-        <Heading>Sub-sub-heading</Heading>
-        <Heading>Sub-sub-heading</Heading>
-        <Heading>Sub-sub-heading</Heading>
-        <Section>
-          <Heading>Sub-sub-heading</Heading>
-          <Heading>Sub-sub-heading</Heading>
-          <Heading>Sub-sub-heading</Heading>
-          <Section>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-          </Section>
-        </Section>
-      </Section>
+      <Heading>My Profile</Heading>
+      <Post title="Hello traveller!" body="Read about my adventures." />
+      <Allposts />
+    </Section>
+  );
+}
+
+function Allposts() {
+  return (
+    <Section>
+      <Heading>Posts</Heading>
+      <RecentPost></RecentPost>
+    </Section>
+  );
+}
+
+function RecentPost() {
+  return (
+    <Section>
+      <Heading>Recent Post</Heading>
+      <Post title="Flavors of Lisbon" body="...those pastéis de nata!" />
+      <Post title="Buenos Aires in the rhythm of tango" body="I loved it!" />
+    </Section>
+  );
+}
+
+type Post = {
+  title: string;
+  body: string;
+};
+
+function Post({ title, body }: Post) {
+  return (
+    <Section isFancy={true}>
+      <Heading>{title}</Heading>
+      <p>
+        <i>{body}</i>
+      </p>
     </Section>
   );
 }
--- src/Section.tsx
+++ src/Section.tsx
@@ -3,12 +3,13 @@ import { LevelContext } from "./LevelContext";
 
 type SectionProps = {
   children: React.ReactNode;
+  isFancy?: boolean;
 };
 
-export default function Section({ children }: SectionProps) {
+export default function Section({ children, isFancy }: SectionProps) {
   const level = useContext(LevelContext);
   return (
-    <section className="section">
+    <section className={"section " + (isFancy ? "fancy" : "")}>
       <LevelContext.Provider value={level + 1}>
         {children}
       </LevelContext.Provider>
