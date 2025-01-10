0b89611bdfca376ced8a710eb6ee1525d38fb7e3
5binn <dhxnejqls13@gmail.com>
2025-01-10 17:23:32 +0900

    useRef05

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { forwardRef, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function Form() {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  function handleClick() {
-    inputRef.current?.focus();
-  }
-
+export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <>
-      <MyInput ref={inputRef} />
-      <button onClick={handleClick}>FOCUS</button>
+      <VideoPlayer
+        isPlaying={isPlaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      ></VideoPlayer>
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? "PAUSE" : "PLAY"}
+      </button>
     </>
   );
 }
 
-const MyInput = forwardRef<HTMLInputElement>((props, ref) => {
-  return <input {...props} ref={ref} />;
-});
+interface VideoPlayerProps {
+  src: string;
+  isPlaying: boolean;
+}
+
+function VideoPlayer({ src, isPlaying }: VideoPlayerProps) {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (isPlaying) {
+      ref.current.play();
+    } else {
+      ref.current.pause();
+    }
+  }, [isPlaying]);
+
+  return <video src={src} ref={ref} loop playsInline></video>;
+}
