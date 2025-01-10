a14db6a291e872d95b66172c48f858701dcd7257
5binn <dhxnejqls13@gmail.com>
2025-01-10 15:50:20 +0900

    useRef04

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,20 @@
-import { useRef } from "react";
+import { forwardRef, useRef } from "react";
 
-export default function App() {
-  const firstRef = useRef<HTMLImageElement | null>(null);
-  const secondRef = useRef<HTMLImageElement | null>(null);
-  const thirdRef = useRef<HTMLImageElement | null>(null);
+export default function Form() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  function handleScrollToFirstCat() {
-    firstRef.current?.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center",
-    });
-  }
-
-  function handleScrollToSecondCat() {
-    secondRef.current?.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center",
-    });
-  }
-
-  function handleScrollToThirdCat() {
-    thirdRef.current?.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center",
-    });
+  function handleClick() {
+    inputRef.current?.focus();
   }
 
   return (
     <>
-      <nav>
-        <button onClick={handleScrollToFirstCat}>Tom</button>
-        <button onClick={handleScrollToSecondCat}>Maru</button>
-        <button onClick={handleScrollToThirdCat}>Jellylorum</button>
-      </nav>
-      <div>
-        <ul>
-          <li>
-            <img
-              src=" https://loremflickr.com/300/200"
-              alt="Tom"
-              ref={firstRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://picsum.photos/300/200"
-              alt="Maru"
-              ref={secondRef}
-            />
-          </li>
-          <li>
-            <img
-              src="https://placehold.co/300x200"
-              alt="Jellylorum"
-              ref={thirdRef}
-            />
-          </li>
-        </ul>
-      </div>
+      <MyInput ref={inputRef} />
+      <button onClick={handleClick}>FOCUS</button>
     </>
   );
 }
+
+const MyInput = forwardRef<HTMLInputElement>((props, ref) => {
+  return <input {...props} ref={ref} />;
+});
