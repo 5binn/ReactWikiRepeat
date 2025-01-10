d41b40845d0cbefa63b0256a69e88b1703958285
5binn <dhxnejqls13@gmail.com>
2025-01-10 10:49:54 +0900

    useRef01

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import AddTask from "./AddTask";
-import TaskList from "./TaskList";
-import TasksProvider from "./TasksProvider";
+import { useEffect, useRef, useState } from "react";
+
+interface VideoPlayerProps {
+  src: string;
+  isPlaying: boolean;
+}
+
+function VideoPlayer({ src, isPlaying }: VideoPlayerProps) {
+  const ref = useRef<HTMLVideoElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      if (isPlaying) {
+        ref.current.play();
+      } else {
+        ref.current.pause();
+      }
+    }
+  }, [isPlaying]);
+  return <video src={src} ref={ref} loop playsInline />;
+}
 
 export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
-    <TasksProvider>
-      <h1>Day off in Tokyo</h1>
-      <AddTask />
-      <TaskList />
-    </TasksProvider>
+    <>
+      <a
+        onClick={() => setIsPlaying(!isPlaying)}
+        style={{ cursor: "pointer", textDecoration: "none" }}
+      >
+        <VideoPlayer
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+          isPlaying={isPlaying}
+        ></VideoPlayer>
+      </a>
+    </>
   );
 }
