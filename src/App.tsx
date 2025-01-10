df11d9290599b9974215f38471c2628659ad32f1
5binn <dhxnejqls13@gmail.com>
2025-01-10 15:22:37 +0900

    useRef03

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,66 @@
 import { useRef } from "react";
 
 export default function App() {
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  const firstRef = useRef<HTMLImageElement | null>(null);
+  const secondRef = useRef<HTMLImageElement | null>(null);
+  const thirdRef = useRef<HTMLImageElement | null>(null);
 
-  function handleClick() {
-    inputRef.current?.focus();
+  function handleScrollToFirstCat() {
+    firstRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
   }
+
+  function handleScrollToSecondCat() {
+    secondRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
+  function handleScrollToThirdCat() {
+    thirdRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
   return (
     <>
-      <input ref={inputRef} />
-      <button onClick={handleClick}>FOCUS</button>
+      <nav>
+        <button onClick={handleScrollToFirstCat}>Tom</button>
+        <button onClick={handleScrollToSecondCat}>Maru</button>
+        <button onClick={handleScrollToThirdCat}>Jellylorum</button>
+      </nav>
+      <div>
+        <ul>
+          <li>
+            <img
+              src=" https://loremflickr.com/300/200"
+              alt="Tom"
+              ref={firstRef}
+            />
+          </li>
+          <li>
+            <img
+              src="https://picsum.photos/300/200"
+              alt="Maru"
+              ref={secondRef}
+            />
+          </li>
+          <li>
+            <img
+              src="https://placehold.co/300x200"
+              alt="Jellylorum"
+              ref={thirdRef}
+            />
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
