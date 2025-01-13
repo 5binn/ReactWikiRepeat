92942773d34f881de172adbe9807a99ddfadd356
5binn <dhxnejqls13@gmail.com>
2025-01-13 16:36:10 +0900

    customHook01

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,28 @@
-import { useEffect, useState } from "react";
-import { createConnection, sendMessage } from "./Chat";
+import { useState, useEffect } from "react";
+import { useOnlineStatus } from "./useOnlineStatus";
 
-const serverUrl = "https://localhost:1234";
+function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
+}
 
-function ChatRoom({ roomId }: any) {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    const connection = createConnection(serverUrl, roomId);
-    connection.connect();
-    return () => connection.disConnect();
-  }, [roomId]);
-
-  function sendClick() {
-    sendMessage(message);
+function SaveBtn() {
+  const isOnline = useOnlineStatus();
+  function handleSaveClick() {
+    console.log("✅ Progress saved");
   }
   return (
-    <>
-      <h1>Welcome to the {roomId} room!</h1>
-      <input value={message} onChange={(e) => setMessage(e.target.value)} />
-      <button onClick={sendClick}>SEND</button>
-    </>
+    <button disabled={!isOnline} onClick={handleSaveClick}>
+      {isOnline ? "Save progress" : "Reconnecting..."}
+    </button>
   );
 }
 
 export default function App() {
-  const [roomId, setRoomId] = useState<string>("general");
-  const [show, setShow] = useState(false);
-
   return (
     <>
-      <label>
-        Choose the room:{" "}
-        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
-          <option value="general">general</option>
-          <option value="music">music</option>
-          <option value="game">game</option>
-        </select>
-      </label>
-      <button onClick={() => setShow(!show)}>{show ? "CLOSE" : "OPEN"}</button>
-      {show && <br />}
-      {show && <ChatRoom roomId={roomId} />}
+      <SaveBtn />
+      <StatusBar />
     </>
   );
 }
