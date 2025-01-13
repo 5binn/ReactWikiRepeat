09da34004c9dcd962f91face3536a5547916b6ac
5binn <dhxnejqls13@gmail.com>
2025-01-13 17:16:44 +0900

    customHook03

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,55 @@
-import { useOnlineStatus } from "./useOnlineStatus";
+import { useEffect, useRef, useState } from "react";
 
-function StatusBar() {
-  const isOnline = useOnlineStatus();
-  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
-}
+function Welcome() {
+  const ref = useRef<HTMLHeadingElement | null>(null);
 
-function SaveButton() {
-  const isOnline = useOnlineStatus();
+  useEffect(() => {
+    const duration = 1000;
+    const node = ref.current;
 
-  function handleSaveClick() {
-    console.log("✅ Progress saved");
-  }
+    let startTime: number | null = performance.now();
+    let frameId: number | null = null;
 
+    function onFrame(now: number) {
+      if (startTime === null || !node) return;
+      const timePassed = now - startTime;
+      const progress = Math.min(timePassed / duration, 1);
+
+      node.style.opacity = progress.toString();
+      if (progress < 1) {
+        frameId = requestAnimationFrame(onFrame);
+      }
+    }
+
+    function start() {
+      if (node) node.style.opacity = "0";
+      startTime = performance.now();
+      frameId = requestAnimationFrame(onFrame);
+    }
+
+    function stop() {
+      frameId && cancelAnimationFrame(frameId);
+      startTime = null;
+      frameId = null;
+    }
+
+    start();
+    return () => stop();
+  }, []);
   return (
-    <button disabled={!isOnline} onClick={handleSaveClick}>
-      {isOnline ? "Save progress" : "Reconnecting..."}
-    </button>
+    <h1 className="welcome" ref={ref}>
+      Welcome
+    </h1>
   );
 }
 
 export default function App() {
+  const [show, setShow] = useState(false);
   return (
     <>
-      <SaveButton />
-      <StatusBar />
+      <button onClick={() => setShow(!show)}>{show ? "remove" : "show"}</button>
+      <br />
+      {show && <Welcome />}
     </>
   );
 }
